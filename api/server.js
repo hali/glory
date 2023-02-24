8b3d819fa31b86ab5869d071e723d320d0ad8ef6
Viktoriia Kuznetcova <hali4qa@gmail.com>
2023-02-25 09:51:57 +1300

    Removed secrets

---
+++ api/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const config = require('config');
+
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
+  port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, './static')));
+
+const mysql = require('mysql');
+// connection configurations
+const mc = mysql.createConnection(config.connection);
+ 
+// connect to database
+mc.connect();
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.listen(port);
+const nocache = require('nocache');
+app.use(nocache());
+app.set("etag", false);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+var routes = require('./app/routes/appRoutes.js'); //importing route
+routes(app); //register the route
