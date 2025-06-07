90e842842ceaaeae6597c90c5d7284d2aca344fd
Viktoriia <hali4qa@gmail.com>
2025-06-07 16:46:06 +1200

    Some wild weirdness

--- api/app/model/db.js
+++ api/app/model/db.js
@@ -1,9 +1,20 @@
 'user strict';
 
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var config = require('../../config');
 
-var pool      =    mysql.createPool(config.pool);  
+// Add specific connection options for mysql2
+const poolConfig = {
+    ...config.pool,
+    // Enable proper authentication method negotiation
+    authPlugins: {
+        mysql_native_password: () => () => {
+            return Buffer.from(config.pool.password);
+        }
+    }
+};
+
+var pool = mysql.createPool(poolConfig);
 
 exports.query=function(query, params, callback){
 	pool.query(query, params, function(err,rows){
